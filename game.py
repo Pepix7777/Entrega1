b23e6b8b5bb2c468511f69e8f6fc5dbce337e8bc
Pepix7777 <126592405+Pepix7777@users.noreply.github.com>
2023-03-16 17:15:14 -0300

    Add files via upload

---
+++ game.py
@@ -0,0 +1,45 @@
+from random import choice, randrange
+from datetime import datetime
+# Operadores posibles
+operators = ["+", "-","*","/"]
+# Cantidad de cuentas a resolver
+times = 5
+# Contador inicial de tiempo.
+# Esto toma la fecha y hora actual.
+result_correcto = 0 
+result_incorrecto = 0 
+init_time = datetime.now()
+print(f"¡Veremos cuanto tardas en responder estas {times} operaciones!")
+for i in range(0, times):
+    # Se eligen números y operador al azar
+    number_1 = randrange(10)
+    number_2 = randrange(10) + 1
+    operator = choice(operators)
+    # Se imprime la cuenta.
+    print(f"{i+1}- ¿Cuánto es {number_1} {operator} {number_2}?")
+    # Le pedimos al usuario el resultado
+    result = float(input("resultado: "))
+    match operator:
+        case "+" :
+            result_cuenta = number_1 + number_2
+        case "-" :
+            result_cuenta = number_1 - number_2
+        case "*" :
+            result_cuenta = number_1 * number_2
+        case "/" :
+            result_cuenta = number_1 / number_2
+    if result == result_cuenta :
+        print("Resultado correcto")
+        result_correcto += 1
+    else :
+        print ("Resultado incorrecto")
+        result_incorrecto += 1
+# Al terminar toda la cantidad de cuentas por resolver.
+# Se vuelve a tomar la fecha y la hora.
+end_time = datetime.now()
+# Restando las fechas obtenemos el tiempo transcurrido.
+total_time = end_time - init_time
+# Mostramos ese tiempo en segundos.
+print(f"\n Tardaste {total_time.seconds} segundos.")
+print (f"\nCantidad de resultados correctos: {result_correcto}")
+print (f"\nCantidad de resultados incorrecto: {result_incorrecto}")
